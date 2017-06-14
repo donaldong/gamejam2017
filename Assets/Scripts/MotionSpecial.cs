4fe398d8bdfdfd117b193aa9be701f5bde6f4de8
Katelynn Jones <katjones419@gmail.com>
2017-06-14 14:45:35 -0700

    Fixed pickup, working on throw

--- Assets/Scripts/MotionSpecial.cs
+++ Assets/Scripts/MotionSpecial.cs
@@ -5,28 +5,35 @@ using UnityEngine;
 public class MotionSpecial : MonoBehaviour
 {
 
-    // Katelynn's AOE Special V3
+    // Katelynn's AOE Specials V3
 
-    public float damage;
-    public float rayLength;
+    //aoe special vars
+    public float damage; 
     public float initialCooldown;
     public float cooldownTime;
     public float rangeOfEffect;
-    public GameObject centerEyeAnchor;
-
+    public GameObject centerEyeAnchor; //to detect height of eye level
     private float timeStamp;
+
+    //thrown vars
+    public GameObject weapon;
+    public GameObject weaponDropped;
+    private bool held;
+
+    //shared vars
     private RaycastHit vision;
 
     private void Start()
     {
         timeStamp = initialCooldown;
+        held = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out vision, rayLength))
+        //sky strike
+        if (held && Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out vision, 10))
         {
             if (vision.collider.tag == "Weapon")
             {
@@ -63,6 +70,35 @@ public class MotionSpecial : MonoBehaviour
                             }
                         }
                     }
+                }
+            }
+        }
+
+        //drop, pick up, and throw
+        if ((OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger, OVRInput.Controller.Touch) || OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.Touch)))
+        {
+            if (held)
+            {
+                GameObject newWeapon = Instantiate(weapon, new Vector3(weapon.transform.position.x, weapon.transform.position.y, weapon.transform.position.z), Quaternion.identity);
+                newWeapon.transform.rotation = weapon.transform.rotation;
+                newWeapon.AddComponent<Rigidbody>();
+                //newWeapon.GetComponent<Rigidbody>().AddForce(newWeapon.transform.forward * 1000); //throw
+                newWeapon.tag = "Weapon";
+                weaponDropped = newWeapon;
+                weapon.SetActive(false);
+                held = false;
+            }
+            else
+            {
+                if (vision.collider.tag == "Weapon")
+                {
+                    Destroy(weaponDropped);
+                    weapon.SetActive(true);
+                    held = true;
+                }
+                else
+                {
+                    Debug.Log("[Pick Up] Not Looking at Weapon");
                 }
             }
         }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,8 +4,6 @@ public class Weapon : MonoBehaviour
 {
     public float impact = 1.0f;
     public float damage = 15.0f;
-    public GameObject weapon;
-    public bool held = true;
 
     protected ParticleSystem _effectParticles;
 
@@ -20,16 +18,5 @@ public class Weapon : MonoBehaviour
         _effectParticles.transform.position = pos;
         _effectParticles.Stop();
         _effectParticles.Play();
-    }
-
-    void Update()
-    {
-        if ((OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger, OVRInput.Controller.Touch)  || OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.Touch)) && held)
-        {
-            GameObject droppedWeapon = Instantiate(weapon, new Vector3(transform.position.x, transform.position.y-2.0f, transform.position.z), Quaternion.identity);
-            droppedWeapon.gameObject.tag = "Weapon";
-            droppedWeapon.gameObject.GetComponent<DroppedWeapon>().weapon = gameObject;
-            gameObject.SetActive(false);
-        }
     }
 }
